2c846a05db9b4ae1ad26bfa7bfc7b003c836c5b4
hadye <hadyelzayady@yahoo.com>
2018-10-27 09:24:07 +0200

    set tile on square, init board,update:isanchor and ishook

--- Scrabble_BackEnd/Board.h
+++ Scrabble_BackEnd/Board.h
@@ -2,13 +2,19 @@
 #include <vector>
 #include "Square.h"
 #include "Constants.h"
+#include <string>
 class Board
 {
-public:
-	Square board[ROWS_COUNT][COLUMNS_COUNT];
-	inline const char& getLetter(int row, int column)const;
-	inline bool isEmptySquare(int row, int column) const;
+  public:
+	Square m_board[ROWS_COUNT][COLUMNS_COUNT];
+	inline const char &getLetter(unsigned short row, unsigned short column) const;
+	inline bool isEmptySquare(unsigned short row, unsigned short column) const; //same as square.isEmpty just another interface
 	//bool isConnected(const Move);
+	Board(const Square board[ROWS_COUNT][COLUMNS_COUNT]);
 	Board();
+	void setTile(char letter, unsigned short row, unsigned short column);
+	void updateAnchors(std::string move); //TODO : parameter should be Move objedut this is for testing
+	inline bool isHook(unsigned short row, unsigned short column) const;
+	inline bool isAnchor(unsigned short row, unsigned short column) const;
 	~Board();
 };
--- Scrabble_BackEnd/Constants.h
+++ Scrabble_BackEnd/Constants.h
@@ -3,3 +3,4 @@ constexpr auto ROWS_COUNT = 15;
 constexpr auto COLUMNS_COUNT = 15;
 constexpr auto BLANK_TILE = '_';
 constexpr auto EMPTY_SQUARE = '\0';
+constexpr auto EXCEPTION_OUT_OF_BOUND = "array out of bound (Board-> isEmpty)";
