3144f5f7be615cc940957c663dc8c6815f24752a
ahmed osman <ahmosman96@gmail.com>
2018-11-22 20:14:28 +0200

    adding index and blank check
    adding index of the letter to be played and to check if this character
    is blank

--- Scrabble_BackEnd/Play.cpp
+++ Scrabble_BackEnd/Play.cpp
@@ -13,9 +13,19 @@ void Play::set_Coordinates(pair<int, int> coordinates)
 
 }
 
-void Play::set_Letter(char Letter)
+void Play::set_Letter(char Letter,int index,bool blank= false)
 {
+	this->Index = index; 
+	this->Blank =  blank;
 	this->Letter = Letter;
+}
+
+int Play::get_Index(int index){
+	return this->Index;
+}
+
+bool Play::isBlank(){
+	return this->Blank;
 }
 
 char Play::get_Letter()
--- Scrabble_BackEnd/Play.h
+++ Scrabble_BackEnd/Play.h
@@ -7,10 +7,14 @@ class Play
 private:
 	char Letter;
 	pair<int, int> coordinates;
+	bool Blank;
+	int Index;
 public:
 	pair<int, int> get_Coordinates();
 	void set_Coordinates(pair<int, int> coordinates);
-	void set_Letter(char Letter);
+	void set_Letter(char Letter,int index,bool blank = false);
+	bool isBlank();
+	int get_Index();
 	char get_Letter();
 	Play();
 	~Play();
