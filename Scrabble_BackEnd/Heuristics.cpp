3a6051425f95ac0713d0107c11d4659bd974b6bc
kariamyasser <38008930+kariamyasser@users.noreply.github.com>
2018-12-01 19:02:53 +0200

    dynamic programming in pre end and midGame

--- Scrabble_BackEnd/Heuristics.cpp
+++ Scrabble_BackEnd/Heuristics.cpp
@@ -1,8 +1,4 @@
-
 #include "Heuristics.h"
-
-
-
 const float vcvalues[8][8] =
 {
 	{  0.0,   0.0,  -1.0,  -2.5,  -5.0,  -8.5, -13.5, -18.5},
@@ -19,6 +15,7 @@ Heuristics::Heuristics()
 {
 
 	ReadMap("double_RL.txt");
+	Read_Leave_DP();
 
 
 }
@@ -46,6 +43,52 @@ void Heuristics::ReadMap(string inDirectory)
 
 		}
 		input.close();
+	}
+
+}
+void Heuristics::Read_Leave_DP()
+{
+	ifstream input("leave_DP.txt");
+	if (!input) cout << "Error occured while opening the file" << endl;
+	else
+	{
+
+		while (!input.eof())
+		{
+			string x;
+			input >> x;
+			double cost;
+			input >> cost;
+			leave_DP[x] = cost;
+		}
+		input.close();
+	}
+}
+void Heuristics::saveToFile(vector<char> data,double cost)
+{
+
+	fstream myfile;
+
+	myfile.open("leave_DP.txt",  ios::app);
+
+	if (!myfile.is_open()) {
+
+		cout << "Error occured while opening the file";
+
+	}
+
+	else
+
+	{
+		for (int i = 0; i < data.size(); i++)
+		{
+			myfile << data[i]  ;
+		}
+
+			myfile << " " << cost << endl;
+		
+		myfile.close();
+
 	}
 
 }
@@ -107,49 +150,9 @@ double Heuristics::endGame(vector<char> estimatedRack, Move move, vector<pair<in
 {
 	double cost = 0.0;
 	vector<Play> plays = move.getPlaysPointer();
-	bool hasQ = false;
-	bool hasZ = false;
-	TileLookUp t;
-
-	for (int i = 0; i < estimatedRack.size(); i++)
-	{
-		if (estimatedRack[i] == 'Q')hasQ = true;
-		if (estimatedRack[i] == 'Z')hasZ = true;
-	}
-	int priority = 1;
-	for (int i = 0; i < plays.size(); i++)
-	{
-		if (hasQ)
-		{
-			for (int k = 0; k < Qpos.size(); k++)
-			{
-				if (plays[i].get_Coordinates() == Qpos[k])
-				{
-					priority = priority + 0.2;
-				}
-
-			}
-
-		}
-		if (hasZ)
-		{
-			for (int k = 0; k < Zpos.size(); k++)
-			{
-				if (plays[i].get_Coordinates() == Zpos[k])
-				{
-					priority = priority + 0.2;
-				}
-
-			}
-		}
-
-		cost += t.getScore(plays[i].get_Letter());
-
-	}
-
-	cost = cost * priority;
-	cost = cost * (1 - (plays.size() / 7)); //short word move has less cost than long word move
-
+	double quality = Qsticking(estimatedRack,move,Qpos,Zpos);
+	cost =  (1 - (plays.size() / 7)); //short word move better than long word move
+	cost = cost * quality;
 	return cost;
 }
 double Heuristics::preEnd(Move move, vector<char>  leave, vector<char> uniqleave)
@@ -158,44 +161,56 @@ double Heuristics::preEnd(Move move, vector<char>  leave, vector<char> uniqleave
 	double cost = 0.0;
 	int vowels = 0;
 	int cons = 0;
+	double synergy;
 	if (!leave.empty())
 	{
 
-		for (int i = 0; i < leave.size(); i++)
+		sort(leave.begin(), leave.end());
+
+		string str(leave.begin(), leave.end());
+		if (leave_DP[str] != 0)
 		{
-			if (leave[i] != BLANK_TILE)
-			{
-				if (leave[i] == 'A' || leave[i] == 'E' || leave[i] == 'I' || leave[i] == 'O' || leave[i] == 'U')
-					vowels++;
-				else
-					cons++;
-			}
+			synergy = leave_DP[str];
+
 		}
-		cost += vcvalues[vowels][cons]; //Reward 
+		else
+		{ 
 
+			TileLookUp t;
+			synergy = calculateDRL(leave);
+			synergy += calculateDRL(uniqleave);
 
-		TileLookUp t;
+			// TODO handle the Q
+			bool holding_bad_tile = false;
+			for (int i = 0; i < move.getPlaysPointer().size(); i++)
+			{
+				if (t.getScore(move.getPlaysPointer()[i].get_Letter()) > 7)
+				{
+					holding_bad_tile = true;
+				}
+			}
+			if ((synergy > 3.0) && !holding_bad_tile) {
+				synergy = synergy + 1.5 * (synergy - 3.0);
+			}
+			//save to file DP here
+			saveToFile(leave,cost);
+			
+		}
+
 		
-		double synergy = calculateDRL(leave);
-		synergy += calculateDRL(uniqleave);
-
-		// TODO handle the Q
-		bool holding_bad_tile = false;
-		for (int i = 0; i < move.getPlaysPointer().size(); i++)
-		{
-			if (t.getScore(move.getPlaysPointer()[i].get_Letter()) > 7)
-			{
-				holding_bad_tile = true;
-			}
-		}
-		if ((synergy > 3.0) && !holding_bad_tile) {
-			synergy = synergy + 1.5 * (synergy - 3.0);
-		}
-
-		cost = cost + synergy;
 	}
-
-
+	cost = cost + synergy;
+	for (int i = 0; i < leave.size(); i++)
+	{
+		if (leave[i] != BLANK_TILE)
+		{
+			if (leave[i] == 'A' || leave[i] == 'E' || leave[i] == 'I' || leave[i] == 'O' || leave[i] == 'U')
+				vowels++;
+			else
+				cons++;
+		}
+	}
+	cost += vcvalues[vowels][cons];
 
 	return cost;
 }
@@ -213,48 +228,121 @@ double Heuristics::midGame(bool first_turn, Move  move, vector<char> leave, vect
 	int vowels = 0;
 	int cons = 0;
 
+	double synergy;
 	if (!leave.empty())
 	{
 
-		for (int i = 0; i < leave.size(); i++)
+		sort(leave.begin(), leave.end());
+		string str(leave.begin(), leave.end());
+		if (leave_DP[str] != 0)
 		{
-			if (leave[i] != BLANK_TILE)
-			{
-				if (leave[i] == 'A' || leave[i] == 'E' || leave[i] == 'I' || leave[i] == 'O' || leave[i] == 'U')
-					vowels++;
-				else
-					cons++;
-			}
+			synergy = leave_DP[str];
 		}
-		cost += vcvalues[vowels][cons];
-
-
-		TileLookUp t;
-		
-		double synergy = calculateDRL(leave);
-		synergy += calculateDRL(uniqleave);
-
-		// TODO handle the Q
-		bool holding_bad_tile = false;
-		for (int i = 0; i < move.getPlaysPointer().size(); i++)
+		else
 		{
-			if (t.getScore(move.getPlaysPointer()[i].get_Letter()) > 7)
-			{
-				holding_bad_tile = true;
-			}
-		}
-		if ((synergy > 3.0) && !holding_bad_tile) {
-			synergy = synergy + 1.5 * (synergy - 3.0);
-		}
+			
+			TileLookUp t;
+			synergy = calculateDRL(leave);
+			synergy += calculateDRL(uniqleave);
 
-		cost = cost + synergy;
+			// TODO handle the Q
+			bool holding_bad_tile = false;
+			for (int i = 0; i < move.getPlaysPointer().size(); i++)
+			{
+				if (t.getScore(move.getPlaysPointer()[i].get_Letter()) > 7)
+				{
+					holding_bad_tile = true;
+				}
+			}
+			if ((synergy > 3.0) && !holding_bad_tile) {
+				synergy = synergy + 1.5 * (synergy - 3.0);
+			}
+			//save to file DP here
+			saveToFile(leave, cost);
+
+		}
 	}
+
+	cost = cost + synergy;
+
+
+	for (int i = 0; i < leave.size(); i++)
+	{
+		if (leave[i] != BLANK_TILE)
+		{
+			if (leave[i] == 'A' || leave[i] == 'E' || leave[i] == 'I' || leave[i] == 'O' || leave[i] == 'U')
+				vowels++;
+			else
+				cons++;
+		}
+	}
+	cost += vcvalues[vowels][cons];
 
 
 
 	return cost;
 }
+void Heuristics::endGame2vals(vector<char> estimatedRack,Move move, vector<pair<int, int>>  Qpos, vector<pair<int, int>>  Zpos, double &maxVal, double &minVal)
+{
 
+	double val1 = Qsticking(estimatedRack, move, Qpos, Zpos);
+	double val2 = endGame(estimatedRack, move, Qpos, Zpos);
+
+	if (val1 >= val2)
+	{
+		maxVal = abs(val1);
+		minVal = val2;
+	}
+	else
+	{
+		maxVal = val2;
+		minVal = val1;
+	}
+}
+double Heuristics::Qsticking(vector<char> estimatedRack, Move move, vector<pair<int, int>>  Qpos, vector<pair<int, int>>  Zpos)
+{
+
+	vector<Play> plays = move.getPlaysPointer();
+	bool hasQ = false;
+	bool hasZ = false;
+	double quality = 1;
+
+	for (int i = 0; i < estimatedRack.size(); i++)
+	{
+		if (estimatedRack[i] == 'Q')hasQ = true;
+		if (estimatedRack[i] == 'Z')hasZ = true;
+	}
+
+	for (int i = 0; i < plays.size(); i++)
+	{
+		if (hasQ)
+		{
+			for (int k = 0; k < Qpos.size(); k++)
+			{
+				if (plays[i].get_Coordinates() == Qpos[k])
+				{
+					quality = quality + 0.2;
+				}
+
+			}
+
+		}
+		if (hasZ)
+		{
+			for (int k = 0; k < Zpos.size(); k++)
+			{
+				if (plays[i].get_Coordinates() == Zpos[k])
+				{
+					quality = quality + 0.2;
+				}
+
+			}
+		}
+	}
+	return quality;
+}
 Heuristics::~Heuristics()
 {
 }
+
+
